002ae06714fa608adb946ca4be80fcb0a7d0ec23
FaithDaka <53012890+FaithDaka@users.noreply.github.com>
2019-11-08 00:36:15 +0300

    Add files via upload

--- bubble.js
+++ bubble.js
@@ -1,11 +1,5 @@
 var arr = [];
 
-function swap(array, first_Index, second_Index){
-    var temp = array[first_Index];
-    array[first_Index] = array[second_Index];
-    array[second_Index] = temp;
-}
-
 function printA(array){
     var str = ""
     for(var i =0; i < array.length; i++){
@@ -14,81 +8,50 @@ function printA(array){
     return str;
 }
 
-function bubble_Sort(arrayB){
-    var before = new Date();
-    var len = arrayB.length,
-        i, j, stop
-
-    for (i= 0; i<len; i++){
-        for(j=0, stop = len-i; j<stop; j++){
-            if (arrayB[j] > arrayB[j+1]){
-                swap(arrayB, j, j+1);
-            }
+function bubble_Sort(arrayB) {
+        this.swap = function(arrayB, index1, index2){
+            var value1 = arrayB[index1]
+            arrayB[index1] = arrayB[index2]
+            arrayB[index2] = value1
         }
-    }
-
-    bubble = arrayB
-    var after = new Date();
-    var time = (after.getMilliseconds()- before.getMilliseconds());
-    document.getElementById("bubbleTime").textContent = time;
-    console.log(time);
-    return bubble;
-}
-
-function Selection_Sort(arrayS){
-
-    var len = arrayS.length , min;
-    var before =new Date();
-        for(var i=0; i<len; i++){
-            min = i;
-        }
-
-        for(var j = i+1; j<len; j++){
-            if(arrayS[j] < arrayS[min]){
-                min = j;
-            }
-        }
-
-        if(min != i){
-            swap(arrayS, i, min);
-        }
-
-        selection = arrayS;
-        var after = new Date();
-        time = (after.getMilliseconds() - before.getMilliseconds());
-        document.getElementById("selectTime").textContent = time;
-        console.log(time);
-        return selection;
-}
-
-function Insertion_Sort(arrayI){
-    before = new Date();
-    for(var i = 1; i<arrayI.length; i++){
-        if(arrayI[i] < arrayI[0]){
-            arrayI.unshift(arrayI.splice(i,1)[0]);
-        }
-        else if(arrayI[i] >arrayI[i-1]){
-            continue;
-        }
-        else{
-            for(var j = 1; j<i; j++){
-                if(arrayI[i] >arrayI[j-1] && arrayI[i]<arrayI[j]){
-                    arrayI.splice(j , 0 ,arrayI.splice(i, 1)[0]);
+        this.bubbleSort = function(){
+            for (var i in [...arrayB.keys()].slice(1, arrayB.length)){ // to run n-1 times
+                for (var i = 0; i < arrayB.length; i++){
+                    // console.log(i)
+                    try{
+                        if (arrayB[i] > arrayB[(i + 1)]){
+                            swap(arrayB, i, i + 1)
+                        }
+                    }
+                    catch(er){
+                        
+                    }
                 }
             }
+            console.log("new array: ", arrayB)
+            return arrayB;
         }
     }
 
-    insertion = arrayI;
-    after = new Date();
-    time = (after.getMilliseconds() - before.getMilliseconds());
-    document.getElementById("insertTime").textContent = time;
-    console.log(time);
-    return insertion;
+
+function InsertionSort(arrayI){
+
+	this.sort = function(){
+		var n = arrayI.length
+	    for (var index = 1; index < n; index ++){
+	        var tmp = arrayI[index]
+	        var j = index
+	        while (j>0 && tmp < arrayI[j-1]){
+	            arrayI[j] = arrayI[j - 1]
+	            j = j - 1
+	        }
+            arrayI[j] = tmp
+	    }
+	    return arrayI;
+	}
 }
 
 function Quick_Sort(arrayQ){
-    before = new Date();
     if(arrayQ.length <= 1){
         return arrayQ;
     }
@@ -96,7 +59,7 @@ function Quick_Sort(arrayQ){
         var left = [];
         var right = [];
         var newArray = [];
-        var pivot = arrayQ.pop();
+        var pivot   = arrayQ.pop();
         var length = arrayQ.length;
 
         for(var i = 0; i<length; i++){
@@ -108,12 +71,30 @@ function Quick_Sort(arrayQ){
             }
         }
         quick = newArray.concat(Quick_Sort(left), pivot, Quick_Sort(right));
-        after = new Date();
-        time = (after.getMilliseconds() - before.getMilliseconds());
-        document.getElementById("quickTime").textContent = time;
-        console.log(time);
         return quick;
     }
+}
+
+function SelectionSort(){
+	this.swap = function(array, index1, index2){
+	    var value1 = array[index1]
+	    array[index1] = array[index2]
+	    array[index2] = value1
+	}
+    
+    this.sort = function(arrayS){
+		var array_size = arrayS.length
+	    for (var index = 0; index < array_size; index ++){
+	        this.swap(
+	        	arrayS, 
+	        	arrayS.indexOf(
+	        		Math.min(...arrayS.slice(index, array_size))
+        		),
+        		index
+	        )
+    	}
+        return arrayS;
+	}
 }
 
 function merger(left, right){
@@ -130,19 +111,15 @@ function merger(left, right){
 }
 
 function Merge_Sort(arrayM, half = (arrayM.length/2)){
-    var before = new Date();
     if(arrayM.length < 2){
         return arrayM;
     }
 
     const left = arrayM.splice(0, half);
     var marge =  merger (Merge_Sort(left), Merge_Sort(arrayM))
-    var after = new Date();
-    time = (after.getMilliseconds() - before.getMilliseconds());
-    document.getElementById("mergeTime").textContent = time;
-    console.log(time);
     return marge;
 }
+
 
 
 function enter(){
@@ -156,31 +133,69 @@ function enter(){
 }
 
 function bubble(){
-    var bubble = bubble_Sort(this.arr)
+    var before = new Date();
+
+    var BubbleSort = new bubble_Sort(arr)
+    var bubble = BubbleSort.bubbleSort(arr);
     document.getElementById("bubbleAnswer").textContent = printA(bubble);
-    console.log(bubble)
+    console.log(bubble);
+
+    var after = new Date();
+    var time = (after.getMilliseconds()- before.getMilliseconds());
+    document.getElementById("bubbleTime").textContent = time;
+    console.log(time);
 }
 
 function selection(){
-    var select = Selection_Sort(this.arr)
+    var before = new Date();
+
+    var selectionSort = new SelectionSort();
+    var select = selectionSort.sort(arr)
     document.getElementById("selectAnswer").textContent = printA(select);
-    console.log(select)
+    console.log(select);
+
+    var after = new Date();
+    var time = (after.getMilliseconds()- before.getMilliseconds());
+    document.getElementById("selectTime").textContent = time;
+    console.log(time);
 }
 
 function insertion(){
-    var insert = Insertion_Sort(this.arr)
+    var before = new Date();
+
+    var insertionSort = new InsertionSort(arr);
+    var insert = insertionSort.sort(arr);
     document.getElementById("insertAnswer").textContent = printA(insert);
-    console.log(insert)
+    console.log(insert);
+
+    var after = new Date();
+    var time = (after.getMilliseconds()- before.getMilliseconds());
+    document.getElementById("insertTime").textContent = time;
+    console.log(time);
 }
 
 function quick(){
-    var quick = Quick_Sort(this.arr)
+    var before = new Date();
+
+    var quick = Quick_Sort(arr)
     document.getElementById("quickAnswer").textContent = printA(quick);
-    console.log(quick)
+    console.log(quick);
+
+    var after = new Date();
+    var time = (after.getMilliseconds()- before.getMilliseconds());
+    document.getElementById("quickTime").textContent = time;
+    console.log(time);
 }
 
 function merge(){
-    var merge = Merge_Sort(this.arr)
+    var before = new Date();
+
+    var merge = Merge_Sort(arr)
     document.getElementById("mergeAnswer").textContent = printA(merge);
     console.log(merge)
+
+    var after = new Date();
+    var time = (after.getMilliseconds()- before.getMilliseconds());
+    document.getElementById("mergeTime").textContent = time;
+    console.log(time);
 }
